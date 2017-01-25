a0ba3e2c369f515b94d24345d7d54fe4dcd58f23
liuyibao <734988964@qq.com>
2017-01-25 11:15:18 +0800

    aliyun ecs controll shell

---
+++ config/config-recommend.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liuyibao
+ * Date: 17-1-24
+ * Time: 下午2:41
+ */
+return [
+    'regionId' => 'cn-hongkong',//cn-qingdao,cn-hongkong
+    'key' => 'xxx',
+    'secret' => 'xxx',
+    'imageId'=> 'ubuntu_160401_64_40G_cloudinit_20161115.vhd',
+    'instanceType' => 'ecs.n1.tiny',//ecs.t1.small
+    'password' => 'xxx'
+];
